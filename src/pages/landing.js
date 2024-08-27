e0e02927c363e2add2965b38a8a69131692c989b
usamaahma <usamaahmad087@gmail.com>
2024-08-27 20:00:24 +0500

    usama routes made

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import HeroSection from '../components/herosection'
-import { Carousel } from 'react-bootstrap'
 import Features from '../components/features'
+import Car from '../components/carousel'
 
 function Landingpage() {
   return (
     <div>
       <HeroSection/>
-      <Carousel/>
       <Features/>
+      <Car />
+
     </div>
   )
 }
