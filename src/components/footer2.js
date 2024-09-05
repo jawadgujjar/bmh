e9ef7443b3c0c6e6d0f61d3e0d3709840cc31dcf
usamaahma <usamaahmad087@gmail.com>
2024-09-05 21:56:43 +0500

    contact us responsiveness

--- src/components/footer2.js
+++ src/components/footer2.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FaHeart } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import './footer2.css';
+import { Col, Row } from 'antd';
 
 const Footer12 = () => {
   return (
@@ -10,58 +11,57 @@ const Footer12 = () => {
         <h1>Growing Businesses Since 2005</h1>
         <h2>Made with <FaHeart />  in 25 cities around the world</h2>
       </div>
-      <div className="footer-columns">
-        <div className="footer-column">
-           
-          <ul>   
+      <Row className="footer-columns">
+        <Col className="footer-column" >
+          <ul>
             <li><Link className="link" to="/home" >ARLINGTON</Link></li>
             <li><Link className="link" to="/home" >ATLANTA</Link></li>
             <li><Link className="link" to="/home" >AUSTIN</Link></li>
             <li><Link className="link" to="/home" >BALTIMORE</Link></li>
             <li><Link className="link" to="/home" >CHICAGO</Link></li>
           </ul>
-        </div>
-        <div className="footer-column">
-           
+        </Col>
+        <Col className="footer-column">
+
           <ul>
-          <li><Link className="link" to="/home" >CLEVELAND</Link></li>
-          <li><Link className="link" to="/home" >DALLAS</Link></li>
-          <li><Link className="link" to="/home" >DENVER</Link></li>
-          <li><Link className="link" to="/home" >HOUSTON</Link></li>
-          <li><Link className="link" to="/home" >JACKSONVILLE</Link></li> 
+            <li><Link className="link" to="/home" >CLEVELAND</Link></li>
+            <li><Link className="link" to="/home" >DALLAS</Link></li>
+            <li><Link className="link" to="/home" >DENVER</Link></li>
+            <li><Link className="link" to="/home" >HOUSTON</Link></li>
+            <li><Link className="link" to="/home" >JACKSONVILLE</Link></li>
           </ul>
-        </div>
-        <div className="footer-column">
-           
+        </Col>
+        <Col className="footer-column">
+
           <ul>
-          <li><Link className="link" to="/home" >KANSAS CITY</Link></li> 
-          <li><Link className="link" to="/home" >LAS VEGAS</Link></li> 
-          <li><Link className="link" to="/home" >LOS ANGELES</Link></li> 
-          <li><Link className="link" to="/home" >LOUISVILLE</Link></li> 
-          <li><Link className="link" to="/home" >NEW YORK CITY</Link></li> 
+            <li><Link className="link" to="/home" >KANSAS CITY</Link></li>
+            <li><Link className="link" to="/home" >LAS VEGAS</Link></li>
+            <li><Link className="link" to="/home" >LOS ANGELES</Link></li>
+            <li><Link className="link" to="/home" >LOUISVILLE</Link></li>
+            <li><Link className="link" to="/home" >NEW YORK CITY</Link></li>
           </ul>
-        </div>
-        <div className="footer-column">
-          
+        </Col>
+        <Col className="footer-column">
+
           <ul>
-          <li><Link className="link" to="/home" >MIAMI</Link></li> 
-          <li><Link className="link" to="/home" >MINNEAPOLIS</Link></li> 
-          <li><Link className="link" to="/home" >PHOENIX</Link></li> 
-          <li><Link className="link" to="/home" >NEW ORLEANS</Link></li> 
-          <li><Link className="link" to="/home" >ORLANDO</Link></li> 
+            <li><Link className="link" to="/home" >MIAMI</Link></li>
+            <li><Link className="link" to="/home" >MINNEAPOLIS</Link></li>
+            <li><Link className="link" to="/home" >PHOENIX</Link></li>
+            <li><Link className="link" to="/home" >NEW ORLEANS</Link></li>
+            <li><Link className="link" to="/home" >ORLANDO</Link></li>
           </ul>
-        </div>
-        <div className="footer-column">
-          
+        </Col>
+        <Col className="footer-column">
+
           <ul>
-          <li><Link className="link" to="/home" >PHILADELPHIA</Link></li> 
-          <li><Link className="link" to="/home" >PORTLAND</Link></li> 
-          <li><Link className="link" to="/home" >SAN ANTONIO</Link></li> 
-          <li><Link className="link" to="/home" >SAN DIEGO</Link></li> 
-          <li><Link className="link" to="/home" >SEATTLE</Link></li> 
+            <li><Link className="link" to="/home" >PHILADELPHIA</Link></li>
+            <li><Link className="link" to="/home" >PORTLAND</Link></li>
+            <li><Link className="link" to="/home" >SAN ANTONIO</Link></li>
+            <li><Link className="link" to="/home" >SAN DIEGO</Link></li>
+            <li><Link className="link" to="/home" >SEATTLE</Link></li>
           </ul>
-        </div>
-      </div>
+        </Col>
+      </Row>
       <div className="footer-bottom">
         <p>&copy; {new Date().getFullYear()} - Brand Marketing Hub / Locations / Write For Us / Refer Thrive / Terms / Privacy Policy / Scam Alert / Sitemap</p>
       </div>
