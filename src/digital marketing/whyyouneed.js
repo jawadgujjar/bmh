0f093dda4605d1e9027391bb3c0777afaca4c8e1
usamaahma <usamaahmad087@gmail.com>
2024-09-13 04:15:38 +0500

    digital marketing page

---
+++ src/digital marketing/whyyouneed.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Whyyouneed() {
+  return (
+    <div className=''>
+        
+    </div>
+  )
+}
+
+export default Whyyouneed
--- src/pages/digitalmarketingmain.js
+++ src/pages/digitalmarketingmain.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import HeroDigitalMarketing from '../digital marketing/hero'
+import Aboutdigital from '../digital marketing/aboutdigital'
+import Whydigital from '../digital marketing/whydigital'
+import Whyyouneed from '../digital marketing/whyyouneed'
 
 function Digitalmarketingmain() {
-  return (
-    <div>digitalmarketingmain</div>
-  )
+    return (
+        <div>
+            <HeroDigitalMarketing />
+            <Aboutdigital />
+            <Whydigital />
+            <Whyyouneed />
+        </div>
+    )
 }
 
 export default Digitalmarketingmain
